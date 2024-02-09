a7a712cc6a0259958fc1314bb245aea563ab94a3
GotoYugo <goto.yugo@outlook.com>
2024-02-09 09:40:52 -0300

    CRXD do banco de Customer

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
-import{ FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomerController } from "./controllers/ListCustomerController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     
@@ -6,4 +9,15 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
         return{ok:true}
     })
     
+    fastify.post("/customer", async(request:FastifyRequest, reply:FastifyReply)=>{
+        return new CreateCustomerController().handle(request,reply)
+    })
+
+    fastify.get("/customers", async(request:FastifyRequest, reply:FastifyReply)=>{
+        return new ListCustomerController().handle(request,reply)
+    })
+
+    fastify.delete("/customer", async(request:FastifyRequest, reply:FastifyReply)=>{
+        return new DeleteCustomerController().handle(request,reply)
+    })
 }
